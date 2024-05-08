a20c251019a96aa5834152477ce80742ecfa6ef0
Rafael <fael.hea@gmail.com>
2024-05-07 22:48:41 -0300

    adicionando novos script basico, sobre python

---
+++ python-basico/exemplo1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+x = 5
+y = "Hello, World!"
+
+#This is a comment.
+print("Hello, World!")
---
+++ python-basico/exemplo2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+This is a comment
+written is
+more than just one line
+"""
+
+print("Hello, World!")
