6d2ca236bae5dbb649f85d71767e1db037dedd70
Rafael <fael.hea@gmail.com>
2024-05-08 22:45:07 -0300

    novos exemplos feitos

---
+++ python-basico/comando-importantes.py
@@ -0,0 +1,12 @@
+
+"""
+Abra o arquivo Python que deseja executar no VSCode.
+Pressione ""Ctrl"" + ""Shift"" + ""P"" para abrir a barra de comandos.
+Digite ""Python: Run Python File in Terminal"" e pressione Enter.
+
+"""
+
+
+#Chamar um comando no terminal linux, para usar parametros segue o exemplo.
+#subprocess.run(["ls", "-l"]) 
+#subprocess.run(["clear"])
---
+++ python-basico/exemplo3.py
@@ -0,0 +1,184 @@
+import subprocess
+
+#exe1
+"""
+x = 5
+y = "John"
+
+print(x)
+print(y)
+
+"""
+
+#exe2
+"""
+x = 4        #x is of type int
+x = "Sally"  # x is now ot type str
+print(x)
+
+"""
+
+#exe3
+"""
+x = str(3)   #x will be '3'
+y = int(3)   # y will be 3
+z = float(3) # z will be 3.0
+
+"""
+
+#exe4
+"""
+x = 5
+y = "John"
+
+print(type(x))
+print(type(y))
+
+"""
+
+#exe5
+"""
+x = "John"
+#is the same as
+x = 'John'
+
+"""
+
+#exe6
+
+"""
+a = 4
+A = "Sally"
+#A will not overwrite a
+"""
+#exe7
+"""
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(x,y,z)
+
+print(x)
+print(y)
+print(z)
+
+"""
+#exe8
+
+"""
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+"""
+
+#exe9
+
+"""
+fruits = ["apple","banana", "cherry"]
+x, y, z = fruits
+print(y)
+print(z)
+print(x)
+
+"""
+
+#exe10
+"""
+x = "Python is awesome"
+print(x)
+
+"""
+
+#exe11
+"""
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+"""
+
+#exe12
+
+"""
+x = "Python"
+y = "is"
+z = "awesome"
+
+print(x + y + z)
+
+"""
+
+#exe13
+
+"""
+x = 5
+y = 10
+print(x + y)
+"""
+
+#exe14
+
+"""
+x = 5
+y = "John"
+
+print(x + y)
+
+"""
+
+#exe 15
+"""
+x = 5
+y = "John"
+print(x ,y)
+"""
+
+#exe16
+
+"""
+x = "awesome"
+
+def myfunc():
+    print("Python is " + x)
+
+myfunc()
+
+"""
+
+#exe17
+"""
+x = "awesome"
+
+def myfunc():
+    x = "fantastic"
+    print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+"""
+
+#exe18
+
+"""
+def myfunc():
+    global x
+    x = "Fantastic"
+
+myfunc()
+
+print("Python is " + x)
+"""
+
+#exe19
+
+x = "awesome"
+def myfunc():
+    global x
+    x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
