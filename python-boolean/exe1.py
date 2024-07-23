b0c394343d10b3d0951a1a903576c181478db14c
Rafael <fael.hea@gmail.com>
2024-07-22 22:06:55 -0300

    Adicionando mais conhecimento de python

---
+++ python-boolean/exe1.py
@@ -0,0 +1,64 @@
+"""
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+"""
+
+"""
+a = 200
+b = 33
+
+if b > a:
+    print("B is greater than A")
+else:
+    print("B is not greater than A")
+    
+"""
+
+"""
+print(bool("Hello"))
+print(bool(15))
+
+"""
+
+"""
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+"""
+
+"""
+class myclass():
+    def __len__(self):
+        return 0
+    
+myobj = myclass()
+print(bool(myobj))
+
+"""
+
+"""
+def myFunction():
+    return True
+
+print(myFunction())
+
+"""
+
+"""
+def myFunction() :
+    return True
+
+if myFunction():
+    print("YES!")
+else:
+    print("NO!")
+    
+"""
+
+x = 200
+print(isinstance(x, int))
