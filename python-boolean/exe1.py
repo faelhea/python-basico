5f51fb8821defa2328bcd6db688d7367b0e35a36
Rafael <fael.hea@gmail.com>
2024-11-19 21:16:30 -0300

    Update function python

--- python-boolean/exe1.py
+++ python-boolean/exe1.py
@@ -97,8 +97,77 @@ def my_function(*kids):
 my_function("Emil", "Tobias", "Linus")
 """
 
+"""
 def my_function(child3, child2, child1):
     print("The yougest child is " + child3)
 
 my_function(child1="Emil", child2="Tobias", child3="Linus")
+"""
+
+"""
+def my_function(**kid):
+    print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes")
+"""
+
+"""
+def my_function(country = "Normay"):
+    print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+"""
+
+"""
+def my_function(food):
+    for x in food:
+        print(x)
+
+fruits = ["apple","banana","cherry"]
+my_function(fruits)
+"""
+
+"""
+def my_function(x):
+    return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+"""
+
+"""
+def myfunction():
+    pass
+"""
+
+"""
+def my_function(x, /):
+    print(x)
+
+my_function(3)
+"""
+
+"""
+def my_function(x):
+    print(x)
+
+my_function(x = 3)
+"""
+
+"""
+def my_function(*, x):
+    print(x)
+
+my_function(x = 3)
+"""
+
+def my_function(a, b, /, *, c, d):
+    print(a + b + c + d)
+
+my_function(5, 6, c = 7,d = 8)
 
